b193fb521fe619e515bccc357758206762dc4d62
Fikir-2411 <56601153+Fikir-2411@users.noreply.github.com>
2020-01-22 20:41:32 +0300

    Finalize the code

--- code.rb
+++ code.rb
@@ -1,11 +1,18 @@
-books = {
+puts "Hello World!"
+
+# The program has a hash, already defined, named as Courses 
+# consisting of Course name and Course ECTS and it allows 
+# users to create a course, update a course's ECTS,
+# delete a course by its name and/or list all the Courses
+
+courses = {
     
-    Wheel_of_time: 4,
-    Mistborn: 5,
-    Alchemist: 7,
+    DBA: 6,
+    Web: 7,
+    Networking: 7,
 }
 
-puts "What would you like?(a)(u)(d)(l)"
+puts "(a - add a course)(u - update a course)(d - delete a course)(l) - list all courses"
 
     choice = gets.chomp
     
@@ -13,19 +20,52 @@ puts "What would you like?(a)(u)(d)(l)"
     
     when "a"
         
-        puts "Enter a book's name"
+        puts "Enter a course's name"
         
         title = gets.chomp
-    if books[title.to_sym] .nil?
+    if courses[title.to_sym] .nil?
         
-        puts "Rate the book"
+        puts "Enter the ECTS:"
         
-        rating = gets.chomp
+        ects = gets.chomp
         
-        books[title.to_sym] = rating.to_i
+        courses[title.to_sym] = ects.to_i
         
-        puts "#{title} is added with #{rating} ratings"
-    else puts "You allready have it"
+        puts "#{title} is added with #{ects} ECTS"
+    else puts "The course exist!"
     end
-    puts books
+    
+    when "u"
+        puts "Which Course you want to Update?"
+        
+        title = gets.chomp
+        
+        if courses[title.to_sym] .nil?
+            puts "Sorry the couse was not found!"
+        else puts "What's the new ECTS?"
+            ects = gets.chomp
+            courses[title.to_sym] = ects.to_i
     end
+    
+    when "d"
+        puts "Which Course do you wish to delete?"
+         title = gets.chomp.to_sym
+         
+         if courses[title.to_sym] .nil?
+             puts "The Course Doesn't Exist"
+        else 
+            courses.delete(title)
+        end
+    
+    when "l"
+        courses.each do |course, ects|
+            puts "#{course}:#{ects}"
+            
+        end
+        
+    else 
+        puts "Error!"
+    end
+    
+    
+puts courses
